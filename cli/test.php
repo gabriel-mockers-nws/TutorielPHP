3b3e2dc5a313d5d7329eb618b7e0cdcd4b98b657
gabriel-mockers-nws <gmockers@normandiewebschool.fr>
2025-01-24 08:58:01 +0100

    object

--- cli/test.php
+++ cli/test.php
@@ -1,3 +1,5 @@
 <?php 
-$fichier = __DIR__ . DIRECTORY_SEPARATOR . 'demo.txt';
-echo file_get_contents($fichier);
+require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR . 'Creneau.php';
+$creneau = new Creneau(9 , 12);
+$creneau2 = new Creneau(14, 16);
+echo "{$creneau->toHTML()} et de {$creneau2->toHTML()}" ;
