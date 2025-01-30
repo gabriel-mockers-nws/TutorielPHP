ea3a6c38f5454af10d5280b29b2cc137f886e027
gabriel-mockers-nws <gmockers@normandiewebschool.fr>
2025-01-30 12:16:49 +0100

    static objetc and heritage

--- elements/footer.php
+++ elements/footer.php
@@ -5,11 +5,12 @@
   <div class ="row">
       <div class="col-md-4">
         <?php
-        require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'functions' . DIRECTORY_SEPARATOR . 'compteur.php';
-        ajouter_vue();
-        $vues = nombre_vues();
+        require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR . 'DoubleCompteur.php';
+        $compteur = new DoubleCompteur(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'compteur');
+        $compteur->incrementer();
+        $vues = $compteur->recuperer();
         ?>
-        Il y a <?= nombre_vues() ?> visite<?php if ($vues > 1): ?>s<?php endif; ?> sur le site
+        Il y a <?= $vues ?> visite<?php if ($vues > 1): ?>s<?php endif; ?> sur le site
       </div>
       <div class="col-md-4">
         <form action="/newsletter.php" method="post" class="form-inline">
